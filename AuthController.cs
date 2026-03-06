5aa19205aa4818c85e61c61cb9e6029f89d7d82b
Sahana Gangaraju <sahanagangaraju06@gmail.com>
2026-03-07 00:09:20 +0530

    Create AuthController.cs

---
+++ AuthController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRMSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
