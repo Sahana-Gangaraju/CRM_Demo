445fd6485fb4cf9d57a92da6e1dc75df2fc8b1d3
Sahana Gangaraju <sahanagangaraju06@gmail.com>
2026-03-06 23:26:11 +0530

    Update CRMDbContext.cs

--- CRMDbContext.cs
+++ CRMDbContext.cs
@@ -19,3 +19,23 @@ namespace CRMSystem.Data
         public DbSet<Interaction> Interactions { get; set; }
     }
 }
+
+PM> Update-Database
+Build started...
+Build succeeded.
+fail: Microsoft.EntityFrameworkCore.Database.Command[20102]
+      Failed executing DbCommand (24ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+      SELECT "MigrationId", "ProductVersion"
+      FROM "__EFMigrationsHistory"
+      ORDER BY "MigrationId";
+Failed executing DbCommand (24ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+SELECT "MigrationId", "ProductVersion"
+FROM "__EFMigrationsHistory"
+ORDER BY "MigrationId";
+Microsoft.EntityFrameworkCore.Migrations[20411]
+      Acquiring an exclusive lock for migration application. See https://aka.ms/efcore-docs-migrations-lock for more information if this takes too long.
+Acquiring an exclusive lock for migration application. See https://aka.ms/efcore-docs-migrations-lock for more information if this takes too long.
+Microsoft.EntityFrameworkCore.Migrations[20402]
+      Applying migration '20260306174726_InitialCreate'.
+Applying migration '20260306174726_InitialCreate'.
+Done.
